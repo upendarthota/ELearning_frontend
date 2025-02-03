290244d4bbe54c052861bf425ad5de7f6fa29ef7
UPENDAR THOTA <127228154+upendarthota@users.noreply.github.com>
2025-02-03 20:02:53 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = "https://eleaarning-server.onrender.com/";
+export const server = "https://eleaarning-server.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
